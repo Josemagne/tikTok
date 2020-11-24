3dcaad7c8cca4df2216c3603d8003a4525387bc4
Jaime <jaime-barakat@hotmail.com>
2020-11-24 16:21:11 +0100

    added bot

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //zelle
 const zellen = document.querySelectorAll('.zelle');
+const background = document.body.style.backgroundColor;
 
 zellen.forEach( zelle => {
   zelle.addEventListener('click', e => {
@@ -7,56 +8,68 @@ zellen.forEach( zelle => {
   });
 });
 
-let times = 1; //how often it was clicked
+let times = 0; //how often it was clicked
+let freeze = false // cannot set any sign
 document.addEventListener('DOMContentLoaded', () => {
   
 });
 
 let saver = []; // the cells that are already taken
+const pair = ['o', 'x'];
 
 const abwechslung = (e) => {
-    if (!saver.includes(e.target.dataset.num)) {
+    if (!saver.includes(e.target.dataset.num) && !freeze) {
+      
       if (times % 2 === 0) { // if even then 'x' 
       e.target.innerText = 'o';
       times++;
       saver.push(e.target.dataset.num);
-      evaluate('o');
+      if (times >= 5) {
+        evaluate('o');
+      }
       }
       else { // else 'o'
       e.target.innerText = 'x';
       times++;
       saver.push(e.target.dataset.num);
-      evaluate('x');
+      if (times >= 5) {
+        evaluate('o');
+      }
     }
+    setTimeout(bot(e.target.innerText), 2000);
     }
 }
 
-const regex = /6|12|15|18|24/;
+
 
 const evaluate = (sign) => {
-  let container = [];
+
+  let container = []; // 
   zellen.forEach( (zelle) => {
     if (zelle.innerText === sign) {
       container.push(zelle.dataset.num);
     }
   });
+
+  if (times === 9) completed('restart');
   container.sort( (a,b) => a - b); // order the container
   container = container.map( elem => {
     return Number.parseInt(elem);
   });
   console.log(container)
   let sum = 0; // sum of the 3 elements
-  let test = [false, false, false];
+  let test = [false, false, false]; // 1. difference 2. difference 3. biggest num is 3,6,9 or 7,8,9 or 7,9
   // test horizontal for 3
   for (let i=0; i<container.length-1; i++) {
     if (Math.abs(container[i]-container[i+1]) === 1) {
       test[i] = true;
     }
   }
-  sum = container[0]+container[1]+container[2];
-  if (regex.test(sum.toString())) test[2] = true;
+  let copyContainer = container.slice(0,3) // take the first 3 nums
+  if (copyContainer[2] === 3 || copyContainer[2] === 6 || copyContainer[2] === 9 ) test[2] = true;
   if (!test.includes(false)) {
     completed(sign);
+    console.log('h')
   }
   else  {
     test = [false, false, false];
@@ -69,10 +82,10 @@ const evaluate = (sign) => {
       test[i] = true;
     }
   }
-  sum = container[0]+container[1]+container[2];
-  if (regex.test(sum.toString())) test[2] = true;
+  if (copyContainer[2] === 7 || copyContainer[2] === 8 || copyContainer[2] === 9 ) test[2] = true;
   if (!test.includes(false)) {
     completed(sign);
+    console.log('v')
   }
   else  {
     test = [false, false, false];
@@ -86,28 +99,76 @@ const evaluate = (sign) => {
       test[i] = true;
     }
   }
-  sum = container[0]+container[1]+container[2];
-  if (regex.test(sum.toString())) test[2] = true;
+  if (copyContainer[2] === 7 || copyContainer[2] === 9 ) test[2] = true;
   if (!test.includes(false)) {
     completed(sign);
   }
   else  {
     test = [false, false, false];
     sum = 0;
+    setTimeout(bot(sign), 1000);
   }
 }
 
 
-let result = document.querySelector('.result');
+let result = document.querySelector('.result p');
 const completed = (sign) => {
-  result.innerText = sign + " gewinnt";
-  setTimeout(restart, 2000);
+  if(sign === 'restart') {
+    freeze = true;
+    result.innerText = 'restart';
+    setTimeout(restart('restart'), 2000);
+    times = 0;
+  }
+  else {
+    freeze = true;
+    result.innerText = sign + " gewinnt";
+    if (sign === pair[0]) background = 'green';
+    else background = 'red';
+    setTimeout(restart(), 2000);
+    times = 0;
+  }
 }
 
-const restart = () => {
+const restart = (text) => {
   // set everything as before
+  if (text === 'restart') {
+  console.log(times)
   result.innerText = '';
   zellen.forEach( zelle => {
     zelle.innerText = '';
-  })
+  });
+  saver = [];
+  freeze = false;
+  background = '';
+  }
+  else {
+  console.log(times)
+  result.innerText = '';
+  zellen.forEach( zelle => {
+    zelle.innerText = '';
+  });
+  saver = [];
+  freeze = false;
+  background = '';
+  }
+  
+}
+
+const bot = (sign) => {
+  let random = randomNumber();
+  let filler = (pair[0] === sign) ? pair[1] : pair[0];
+  zellen[random].innerText = filler;
+}
+
+const randomNumber = () => {
+  let random = Math.floor(Math.random() * 8);
+  if (saver.includes(random.toString())) {
+    console.log(typeof random.toString())
+    randomNumber();
+  }
+  
+  else {
+    console.log(random, zellen[random]);
+    return random;
+  }
 }
